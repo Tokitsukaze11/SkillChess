d1b1edcb4c0ebbebf3e3f6ac755c143f4c7b3cc2
Tokitsukaze11 <you05015@gmail.com>
2024-05-29 19:42:32 +0900

    Mouse Over Animation

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : Singleton<GameManager>
     public Camera mainCamera;
     public event Action OnTurnEnd;
     public Func<bool> IsPlayerTurn;
+    private int _targetFPS = 60;
+    public int TargetFPS => _targetFPS;
     public void TurnEnd()
     {
         OnTurnEnd!.Invoke();
@@ -15,5 +17,6 @@ public class GameManager : Singleton<GameManager>
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Confined;
+        Application.targetFrameRate = _targetFPS;
     }
 }
--- Assets/Scripts/PopupDescriptController.cs
+++ Assets/Scripts/PopupDescriptController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,15 +17,20 @@ public class PopupDescriptController : MonoBehaviour
     
     public TextMeshProUGUI _descriptionText;
     public Image _descriptionPopupImage;
+    private Coroutine _panelAnimCoroutine = null;
+    private RectTransform _panelRectTransform;
     private void Awake()
     {
         _movePopup.OnMouseOverPopup += PopupControl;
         _attackPopup.OnMouseOverPopup += PopupControl;
         _defendPopup.OnMouseOverPopup += PopupControl;
         _skillPopup.OnMouseOverPopup += PopupControl;
+        _panelRectTransform = _descriptionPopupImage.rectTransform;
     }
     private void Start()
     {
+        _descriptionPopupImage.gameObject.SetActive(false);
+        _descriptionPopupImage.rectTransform.sizeDelta = new Vector2(800, 0);
         PopupControl(false);
     }
     private void PopupControl(bool isOn,string description = null)
@@ -32,15 +39,48 @@ public class PopupDescriptController : MonoBehaviour
 
         Action UIAction = () =>
         {
+            Coroutine panelAnimCoroutine = null;
             if (isOn)
             {
-                _descriptionPopupImage.gameObject.SetActive(true);
-                _descriptionPopupImage.rectTransform.DOSizeDelta(new Vector2(800, 230), 0.5f);
+                if (_panelAnimCoroutine != null)
+                {
+                    StopCoroutine(_panelAnimCoroutine);
+                }
+                panelAnimCoroutine = StartCoroutine(Co_PanelAnim(true));
             }
             else
-                _descriptionPopupImage.rectTransform.DOSizeDelta(new Vector2(800, 0), 0.1f).OnComplete(
-                    () => _descriptionPopupImage.gameObject.SetActive(false));
+            {
+                if (_panelAnimCoroutine != null)
+                {
+                    StopCoroutine(_panelAnimCoroutine);
+                }
+                panelAnimCoroutine = StartCoroutine(Co_PanelAnim(false));
+            }
+            _panelAnimCoroutine = panelAnimCoroutine;
         };
         UIManager.Instance.UpdateUI(UIAction);
+    }
+    private IEnumerator Co_PanelAnim(bool isOn)
+    {
+        Vector2 targetSize = isOn ? new Vector2(800, 230) : new Vector2(800, 0);
+        if(isOn)
+            _descriptionPopupImage.gameObject.SetActive(true);
+
+        float time = 0;
+        int frame = GameManager.Instance.TargetFPS;
+        float duration = isOn ? 4f * (frame/60f) : 1f * (frame/60f);
+
+        while (true)
+        {
+            time += Time.deltaTime;
+            _panelRectTransform.sizeDelta = Vector2.Lerp(_panelRectTransform.sizeDelta, targetSize, time/duration);
+            if (Vector2.Distance(_panelRectTransform.sizeDelta, targetSize) < 0.1f)
+            {
+                _panelRectTransform.sizeDelta = targetSize;
+                if (!isOn) _descriptionPopupImage.gameObject.SetActive(false);
+                yield break;
+            }
+            yield return null;
+        }
     }
 }
